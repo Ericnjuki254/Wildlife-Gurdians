b2b2c0472f21e8ad3e4662006ca4bead0c435f5b
Eric <njukieric233@gmail.com>
2025-07-01 15:50:24 +0300

    installed animation on scroll library

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -6,7 +6,7 @@
         />
         <div class="relative z-10 flex flex-col items-center justify-centerw-full h-full">
             <h1 class="text-4xl font-bold mb-4 text-white text-center">Protecting our planet's Wildlife</h1>
-            <p class="mb-6 text-gray-200 font-semibold text-center">Every species plays a vital role in our ecosystem. Join us in our mission to preserve biodiversity and create a sustainable future for all living beings.</p>
+            <p class="mb-6 text-white font-semibold text-center">Every species plays a vital role in our ecosystem. Join us in our mission to preserve biodiversity and create a sustainable future for all living beings.</p>
         </div>
     </section>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/style.css'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+AOS.init()
 
 createApp(App).mount('#app')
