f3089de56b996ee0d1893fc23cced25ca4a41fa7
Eric <njukieric233@gmail.com>
2025-07-08 13:14:08 +0300

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <Header />
   <HeroSection />
+  <Statistics />
 </template>
 
 <script setup>
 
 import Header from './components/Header.vue';
 import HeroSection from './components/HeroSection.vue';
+import Statistics from './components/Statistics.vue';
 
 </script>
 
