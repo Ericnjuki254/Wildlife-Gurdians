df5b71b604a9b04edd815e40af5c52c81c8b3b2f
erico <njukieric233@gmail.com>
2025-09-08 15:53:46 +0300

    updates

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
-import './assets/style.css'
+import './assets/main.css'
+
 
 import AOS from 'aos'
 import 'aos/dist/aos.css'
